37f85cb320d393fff62915cf36966771fbbdd9fb
dpslwk <dps.lwk@gmail.com>
2020-01-09 19:19:23 +0000

    TSB: Split loan repayments

--- tsb-import.py
+++ tsb-import.py
@@ -71,7 +71,12 @@ logger.info("Into GnuCash book: {}".format(bookPath))
 
 with open_book(bookPath, readonly=False) as book:
     tsbAccount = book.accounts(fullname="Assets:Current Assets:TSB Account")
+    # grab extra accounts we need
+    g456Account = book.accounts(fullname="Expenses:Bizspace Rent:G4,5,6")
+    electricAccont = book.accounts(fullname="Expenses:Utilities:Electric")
+    intrestAccount = book.accounts(fullname="Expenses:Member Loan Repayments:Interest Payments")
     gbp = tsbAccount.commodity
+
     importCount = 0
     # expect one JSON transaction per line
     # {
@@ -111,39 +116,66 @@ with open_book(bookPath, readonly=False) as book:
             amount = Decimal(transaction['amount'])/100
             createdAt = isoparse(transaction['date']).astimezone(pytz.timezone("Europe/London"))
 
+            splits = []
+
             if (transaction['transferAccount'] == 'Expenses:Bizspace Rent:F6' and (-1*transaction['amount']) > (f6Rent+g456Rent)):
                 # pre slipt the rent
-                # grab extra accounts we need
-                g456Account = book.accounts(fullname="Expenses:Bizspace Rent:G4,5,6")
-                electricAccont = book.accounts(fullname="Expenses:Utilities:Electric")
-
                 # prep rent ammounts
                 f6Amount = Decimal(f6Rent)/100
                 g456Amount = Decimal(g456Rent)/100
+
                 # calculate electric amount
                 electricAmount = Decimal(transaction['amount'] + f6Rent + g456Rent)/100
 
-                Transaction(currency=gbp,
-                            enter_date=createdAt,
-                            post_date=createdAt.date(),
-                            num=hashHex,
-                            description=transaction['description'],
-                            splits=[
-                                Split(account=tsbAccount, value=amount),
-                                Split(account=transferAccount, value=f6Amount), #F6
-                                Split(account=g456Account, value=g456Amount),
-                                Split(account=electricAccont, value=-1*electricAmount)
-                            ])
+                splits=[
+                    Split(account=tsbAccount, value=amount),
+                    Split(account=transferAccount, value=f6Amount), #F6
+                    Split(account=g456Account, value=g456Amount),
+                    Split(account=electricAccont, value=-1*electricAmount)
+                ];
+            elif (transaction['transferAccount'] == 'Liabilities:Membership Loan Payable'):
+                # per split loan repayments
+                # 20.83   3.34    24.17
+                # 83.33   13.34   96.67
+                # 41.67   6.66    48.33
+                # 104.17  16.66   120.83
+                payableAmount = -1*amount
+                intrestAmount = Decimal(0)
+                if (transaction['amount'] == -2417):
+                    payableAmount = Decimal(2083)/100
+                    intrestAmount = Decimal(334)/100
+                elif (transaction['amount'] == -9667):
+                    payableAmount = Decimal(8333)/100
+                    intrestAmount = Decimal(1334)/100
+                elif (transaction['amount'] == -4833):
+                    payableAmount = Decimal(4167)/100
+                    intrestAmount = Decimal(666)/100
+                elif (transaction['amount'] == 1-2083):
+                    payableAmount = Decimal(10417)/100
+                    intrestAmount = Decimal(1666)/100
+                else:
+                    logger.warn("Unmatched loan repayment amount")
+
+                splits=[
+                    Split(account=tsbAccount, value=amount),
+                    Split(account=transferAccount, value=payableAmount),
+                    Split(account=intrestAccount, value=intrestAmount)
+                ];
             else:
-                Transaction(currency=gbp,
-                            enter_date=createdAt,
-                            post_date=createdAt.date(),
-                            num=hashHex,
-                            description=transaction['description'],
-                            splits=[
-                                Split(account=tsbAccount, value=amount),
-                                Split(account=transferAccount, value=-1*amount)
-                            ])
+                # just the normal splits
+                splits = [
+                    Split(account=tsbAccount, value=amount),
+                    Split(account=transferAccount, value=-1*amount)
+                    ];
+
+            # now we have the splits we can create the trasnaction
+            Transaction(currency=gbp,
+                        enter_date=createdAt,
+                        post_date=createdAt.date(),
+                        num=hashHex,
+                        description=transaction['description'],
+                        splits=splits
+                        )
 
             # save the book
             if not book.is_saved:
